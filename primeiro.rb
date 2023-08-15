c9add3b5f87b64a7aa5928ab4e75b3052577082b
dssalim <104309303+dssalim@users.noreply.github.com>
2023-08-15 15:16:16 -0300

    feat:Resoluçao do desafio

---
+++ primeiro.rb
@@ -0,0 +1,13 @@
+
+puts "Informe o seu Nome:"
+
+nome = gets.chomp()
+
+puts "Informe o seu Sobrenome:"
+
+sobrenome = gets.chomp()
+
+puts "Informe o sua Idade:"
+idade = gets.chomp()
+
+puts " #{nome} #{sobrenome} | #{idade} anos "
